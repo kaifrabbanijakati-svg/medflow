73d2653f2ba0fd57176025698474873f0ed1e15c
Kaif <kaifrabbanijakati@gmail.com>
2026-05-05 04:41:26 +0530

    final fix

--- src/api.js
+++ src/api.js
@@ -1,16 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  // Backend URL on Render
-  baseURL: import.meta.env.VITE_API_URL || 'https://medflow-aow2.onrender.com',
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: "https://medflow-aow2.onrender.com"
 });
 
 export default api;
